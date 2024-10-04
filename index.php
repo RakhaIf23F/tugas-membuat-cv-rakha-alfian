b3accd7ce3da13a22c5eeed22fd91896e7fdeb7a
Rakha <if23.rakhaalfian@mhs.ubpkarawang.ac.id>
2024-10-04 21:49:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <meta name="view"
+content="withd=device-width, initial-scale=1.0">
+    <linkrel="stylesheet"
+    href="style.css">
+    <title>Cv Rakha Alfian</title>
+</head>
+<body>
+   
+    <header>
+       <img src="WhatsApp Image 2024-10-03 at 19.30.26_c0412161.jpg" class="Profile-pic">
+       <h1>Rakha Alfian</h1>
+       <p>rakhaalfian120@gmail.com | Telepon:08-1294-7671-52 | Jatisari Kab.Karawang</p>
+       <a href="https://www.instagram.com/__.rkh4xyz" target="_blank" class="contact-button">instagram</a>
+    </header>
+    
+    <section> 
+        <h2>About Me</h2>
+        <p>Saya Seorang Mahasiswan Di Universitas Buana Perjuangan Karawang Prodi Informatika,Fakultas Ilmu Komputer,Aktifitas Terakhir Saat Ini Adalah Mencari Pengalaman Untuk Bekerja,Saya Tertarik Mempelajari Bahasa Pemograman Java Script Python Dan Kelebihan Saya Mampu Bekerja Sama Dengan Tim Maupun Individu Dan Disiplin.</p>
+    </section>
+    
+    <section id="Education">
+    <ul>
+        <h2> Pendidikan Terakhir</h2>
+        <li><a href="https://smktrimitra.sch.id/" target="_blank" class="">SMKS TRI MITRA KARAWANG:2020-2023</a></li>
+        <li><a href="https://ubpkarawang.ac.id/" target="_blank"  >UNIVERSITAS BUANA PERJUANGAN KARAWANG:2023-SEKARANG</a></li>
+
+    </ul>
+</section>
+
+<section id="Work Experience">
+        <h2>Pengalaman Kerja</h2>
+        <ul>
+           <li>Barista Kaum Kopi</li>
+           <li>Pelatihan Barista Dan Pastry Di Balai Latihan Kerja Kab.Karawang</li>
+           <li>Operator Produksi Di Pt.Abimanyu Sekar Nusantara</li>
+           <li>Asisten Projek Director Di Perusahaan Karya Insirasi Indonesia</li>
+        </ul>
+    </section>
+
+    <section id="Pengalaman">
+        <h2>Pengalaman</h2>
+        <ul>
+            <li>Pernah Menjadi Sosialisator Pelatihan Bahasa Inggris</li>
+            <li>Menjadi Panitia Seminar Bahasa Inggris</li>
+        </ul>
+    </section>
+
+    <footer>
+        <p>Terimakasih Telah Melihat Cv Saya</p>
+    </footer>
+    </body>
+    </html>
+
